8cd26d36ae1203755ca439cf9535f0438bef4a41
Benjja006 <sepulvedabenjamin464@gmail.com>
2025-04-21 13:30:07 -0400

    avances de la clase de arreglos

--- arreglos.py
+++ arreglos.py
@@ -1,1 +1,7 @@
 #hola comm
+estudiantes = [
+{ "nombre" : "maxi" , "edad" : 20 , "curso" : "2A" },
+{ "nombre" : "richard" , "edad" : 19 , "curso" : "2A"}
+]
+print(estudiantes)
+print(estudiantes[1]["curso"])
